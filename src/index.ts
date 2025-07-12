de87882283f966a35167bddda0ffdbc418ed472e
Abdelrahman <youssifehab218@gmail.com>
2025-07-12 12:30:08 +0300

    create update, delete and lists functions in task class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { Task } from "./task";
 
-const task = new Task();
-task.add("Task 1");
+async function run() {
+  const task = new Task();
+  console.log(await task.deleteById(6));
+  console.log(await task.markDone(6));
+  console.log(await task.markInProgress(5));
+  console.log(await task.list());
+  console.log(await task.listByStatus("done"));
+  console.log(await task.listByStatus("in-progress"));
+  console.log(await task.listByStatus("todo"));
+  console.log(await task.add("hello"));
+  console.log(await task.update(2, "hello"));
+}
+
+run();
--- src/task.ts
+++ src/task.ts
@@ -12,12 +12,13 @@ interface taskData {
 }
 
 export class Task {
+  constructor(
+    private filePath: string = path.join(__dirname, "..", "tasks.json")
+  ) {}
   public async add(description: string) {
-    const filePath = path.join(__dirname, "..", "tasks.json");
-
     let tasksArr: taskData[] = [];
-    if (fs.existsSync(filePath)) {
-      const raw = await fs.promises.readFile(filePath, "utf8");
+    if (fs.existsSync(this.filePath)) {
+      const raw = await fs.promises.readFile(this.filePath, "utf8");
       tasksArr = raw ? JSON.parse(raw) : [];
     }
 
@@ -33,11 +34,151 @@ export class Task {
     tasksArr.push(task);
 
     await fs.promises.writeFile(
-      filePath,
+      this.filePath,
       JSON.stringify(tasksArr, null, 2),
       "utf8"
     );
 
-    console.log("Task create successfully ✅");
+    return "Task created successfully ✅";
+  }
+
+  public async update(id: number, newDescription: string) {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const raw = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = raw ? JSON.parse(raw) : [];
+    }
+
+    if (!tasksArr[id - 1]) {
+      return "This task doesn't exist ❌";
+    }
+
+    let task = tasksArr[id - 1];
+    task.description = newDescription;
+    task.updatedAt = new Date();
+
+    await fs.promises.writeFile(
+      this.filePath,
+      JSON.stringify(tasksArr, null, 2),
+      "utf8"
+    );
+
+    return "Task updated successfully ✅";
+  }
+
+  public async deleteById(id: number) {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const row = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = row ? JSON.parse(row) : [];
+    }
+
+    if (!tasksArr[id - 1]) {
+      return "This task doesn't exist ❌";
+    }
+
+    delete tasksArr[id - 1];
+
+    await fs.promises.writeFile(
+      this.filePath,
+      JSON.stringify(tasksArr, null, 2),
+      "utf8"
+    );
+
+    return "Task deleted successfully ✅";
+  }
+
+  public async markInProgress(id: number) {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const row = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = row ? JSON.parse(row) : [];
+    }
+
+    if (!tasksArr[id - 1]) {
+      return "This task doesn't exist ❌";
+    }
+
+    let task = tasksArr[id - 1];
+    task.status = "in-progress";
+    task.updatedAt = new Date();
+
+    await fs.promises.writeFile(
+      this.filePath,
+      JSON.stringify(tasksArr, null, 2),
+      "utf8"
+    );
+
+    return "Status of the task updated successfully ✅";
+  }
+
+  public async markDone(id: number) {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const row = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = row ? JSON.parse(row) : [];
+    }
+
+    if (!tasksArr[id - 1]) {
+      return "This task doesn't exist ❌";
+    }
+
+    let task = tasksArr[id - 1];
+    task.status = "done";
+    task.updatedAt = new Date();
+
+    await fs.promises.writeFile(
+      this.filePath,
+      JSON.stringify(tasksArr, null, 2),
+      "utf8"
+    );
+
+    return "Status of the task updated successfully ✅";
+  }
+
+  public async list() {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const row = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = row ? JSON.parse(row) : [];
+    }
+
+    let result = [];
+    for (let task of tasksArr) {
+      if (task) {
+        result.push({
+          id: task.id,
+          description: task.description,
+          status: task.status,
+        });
+      }
+    }
+    return result;
+  }
+
+  public async listByStatus(status: taskStatus) {
+    let tasksArr: taskData[] = [];
+
+    if (fs.existsSync(this.filePath)) {
+      const row = await fs.promises.readFile(this.filePath, "utf8");
+      tasksArr = row ? JSON.parse(row) : [];
+    }
+
+    let result = [];
+    for (let task of tasksArr) {
+      if (task && status === task.status) {
+        result.push({
+          id: task.id,
+          description: task.description,
+          status: task.status,
+        });
+      }
+    }
+    return result;
   }
 }
